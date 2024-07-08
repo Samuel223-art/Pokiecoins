02a7af89e44b613bce790a7857856206125387bf
Samuel223-art <wilsosamuel6@gmail.com>
2024-07-08 04:25:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', () => {
+  window.open('https://youtube.com/@pokiecoinschannel?si=ML6qTG03W_kOEaLt', '_blank');
+});
+
+
+
+
+const butt = document.getElementById('myButt');
+
+butt.addEventListener('click', () => {
+  window.open('https://x.com/samue78527?t=8Ndapzw86jEwOXgIFqutoQ&s=08', '_blank');
+});
+
+
+
+
+    
+const redBox = document.getElementById('redBox');
+
+button.addEventListener('click', () => {
+  setTimeout(() => {
+    redBox.style.display = 'block';
+  }, 15000); // 15000ms = 15s
+});
+
+
+
+
+const myButt = document.getElementById('myButt');
+
+myButt.addEventListener('click', () => {
+  setTimeout(() => {
+    redBo.style.display = 'block';
+  }, 15000); // 15000ms = 15s
+});
+
+
+
+
+
+
+
+
+const back = document.getElementById('back');
+
+button.addEventListener('click', () => {
+  window.open('index.html', '_blank');
+});
